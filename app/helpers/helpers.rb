825aad9b01c353661704c9c6e8eea2275577d668
okorkov <alex.okorkov@gmail.com>
2020-12-17 14:28:02 -0800

    commit tree 481d1388db864063fc2800c6264fc1c2700f0237

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+  def self.current_user(hash)
+    @user = User.find(hash[:user_id])
+    @user
+  end
+
+  def self.is_logged_in?(hash)
+    if hash[:user_id] != nil
+      true
+    else
+      false
+    end
+  end
+
 end
